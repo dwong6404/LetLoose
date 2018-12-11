f6ee9b78140528996906a80e56452143b625e22e
dwong6404 <38169733+dwong6404@users.noreply.github.com>
2018-12-10 23:29:33 -0600

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_Emw7yF2Ez', 
+        userPoolClientId: '1t16pf3hud45gj9otre4mjadcl', 
+        region: 'us-east-1' 
+    },
+    api: {
+        invokeUrl: 'https://dyh5alofo1.execute-api.us-east-1.amazonaws.com/prod',
+    }
+};
